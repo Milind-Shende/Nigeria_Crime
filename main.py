6acb66aee7d5690501e6fe8f1c55949d3c997dc4
Milind Shende <milind.shende24@rediffmail.com>
2023-08-07 16:57:24 +0530

    setup.py and packages installation, Data_ingestion and data_validation stage completed

---
+++ main.py
@@ -0,0 +1,28 @@
+from NigeriaMLflow import logger
+from NigeriaMLflow.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from NigeriaMLflow.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+# from NigeriaMLflow.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+# from NigeriaMLflow.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+# from NigeriaMLflow.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
+
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
