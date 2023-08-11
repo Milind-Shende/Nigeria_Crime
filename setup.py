9a5f2834a95148ab3b09bdcae0554b2a6e009163
Milind Shende <milind.shende24@rediffmail.com>
2023-08-11 13:47:26 +0530

    Updated

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 from setuptools import find_packages,setup
 from typing import List
 
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
 REQUIREMENT_FILE_NAME="requirements.txt"
 HYPHEN_E_DOT = "-e ."
 
+__version__ = "0.0.0"
+
+REPO_NAME = "Nigeria_Crime"
+AUTHOR_USER_NAME = "Milind-Shende"
+SRC_REPO = "NigeriaMLflow"
+AUTHOR_EMAIL = "milind.shende24@rediffmail.com"
 
 def get_requirements()->List[str]:
 
@@ -17,11 +27,20 @@ def get_requirements()->List[str]:
     return requirement_list
 
 
+
 setup(
-    name="Nigeria_crime",
-    version="0.0.1",
-    author="Omdena",
-    author_email="milind.shende24@rediffmail.com",
-    packages = find_packages(),
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for ml app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
     install_requires=get_requirements(),
-)
+    )
